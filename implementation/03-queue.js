d584f45eeb5dad5ec1e23203ddc647b83b6a5dbb
nikita <4luckynikita@gmail.com>
2024-02-07 16:30:53 -0800

    broken but one test case off

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -10,7 +10,20 @@ class Queue {
 
     enqueue(val) {
         // Add node to end of queue (linked list)
-
+        let newNode = new SinglyLinkedNode(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+            this.length++;
+            return this.length;
+        }
+        else{
+            
+            this.tail = newNode;
+            if(this.length === 1) this.head.next = newNode;
+            this.length++;
+            return this.length;
+        }
         // Your code here 
 
         // Write your hypothesis on the time complexity of this method here
@@ -18,8 +31,21 @@ class Queue {
 
     dequeue() {
         // Remove node from front of queue (linked list)
-
+        if(this.length === 0) return null;
         // Your code here 
+        if(this.length === 1){
+            let oldVal = this.head.value;
+            this.head = null;
+            this.tail = null;
+            this.length--;
+            return oldVal;
+        }
+        
+        let oldVal = this.head.value;
+        this.head = this.head.next;
+        console.log(this.head);
+        this.length--;
+        return oldVal;
 
         // Write your hypothesis on the time complexity of this method here
     }
